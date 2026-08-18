fce4cb516859ac2107343c576d7833620a0bd21d
shreyaslande <shreyaslande200@gmail.com>
2026-08-18 23:57:43 +0530

    ch

--- server/index.js
+++ server/index.js
@@ -109,6 +109,7 @@ app.get("/api/health", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
